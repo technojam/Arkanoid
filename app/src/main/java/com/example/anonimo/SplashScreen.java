655dead875287d398fef21e7ce20ded22740d3a8
Areeb786123 <areebrockzz786@gmail.com>
2021-08-29 11:42:12 +0530

    first backend

--- app/src/main/java/com/example/anonimo/SplashScreen.java
+++ app/src/main/java/com/example/anonimo/SplashScreen.java
@@ -8,19 +8,36 @@ import android.os.Handler;
 
 import com.example.anonimo.activities.Login;
 import com.example.anonimo.activities.introActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreen extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+   // private String user ;
+    private Object FirebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser = mAuth.getCurrentUser();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, introActivity.class));
+//                startActivity(new Intent(SplashScreen.this, introActivity.class));
+//
 
+                if (FirebaseUser != null){
+                    startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                }
+                else{
+                    startActivity(new Intent(SplashScreen.this,introActivity.class));
+                }
 
 
 
